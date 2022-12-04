549746b19f2c508b71ad3bcca1cb0e352a8dea78
Undefined Name <74594673+udfn@users.noreply.github.com>
2022-12-04 13:13:11 +0200

    Add option to show a timer

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <cairo.h>
 #include <signal.h>
+#include <sys/timerfd.h>
+#include <time.h>
+#include <unistd.h>
 #include <wayland-client.h>
 #include <nwl/nwl.h>
 #include <nwl/surface.h>
@@ -10,6 +13,10 @@
 
 #define DASH_LENGTH 12.0
 struct nwl_surface *main_surface;
+
+bool rect_show_timer = false;
+struct timespec time_start;
+int time_update_fd = -1;
 
 static void rect_render(struct nwl_surface *surface, cairo_surface_t *cairo_surface) {
 	cairo_t *cr = cairo_create(cairo_surface);
@@ -33,6 +40,39 @@ static void rect_render(struct nwl_surface *surface, cairo_surface_t *cairo_surf
 	nwl_surface_swapbuffers(surface, 0, 0);
 }
 
+static void rect_sub_render(struct nwl_surface *surface, cairo_surface_t *cairo_surface) {
+	cairo_t *cr = cairo_create(cairo_surface);
+	cairo_set_source_rgba(cr, 0.2, 0.025, 0.0, 0.85);
+	cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+	cairo_paint(cr);
+	cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
+	char timestr[8];
+	struct timespec new_time;
+	clock_gettime(CLOCK_MONOTONIC, &new_time);
+	int secdiff = new_time.tv_sec-time_start.tv_sec;
+	int nsecdiff = new_time.tv_nsec-time_start.tv_nsec;
+	if (nsecdiff < 0) {
+		nsecdiff += 1000000000;
+		secdiff -= 1;
+	}
+	nsecdiff /= 100000000;
+	snprintf(timestr, 8, "%02d:%02d.%d", secdiff / 60, secdiff % 60, nsecdiff);
+	cairo_select_font_face(cr, "monospace", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+	cairo_set_source_rgba(cr, 0.9, 0.9, 0.9, 1.0);
+	cairo_set_font_size(cr, 14);
+	cairo_move_to(cr, 6, 16);
+	cairo_show_text(cr, timestr);
+	cairo_destroy(cr);
+	nwl_surface_swapbuffers(surface, 0, 0);
+}
+
+static void handle_timer(struct nwl_state *state, void *data) {
+	struct nwl_surface *sub = data;
+	nwl_surface_set_need_draw(sub, false);
+	char expire_count[8];
+	read(time_update_fd, expire_count, 8);
+}
+
 struct nwl_output *find_output(struct nwl_state *state, int32_t x, int32_t y) {
 	struct nwl_output *output;
 	wl_list_for_each(output, &state->outputs, link) {
@@ -48,9 +88,36 @@ static void handle_term() {
 	main_surface->state->num_surfaces = 0; // This hack again :/
 }
 
+static void print_usage(char *arg) {
+	printf("Usage: %s [options] rect\n", arg);
+	puts("(where rect is a rectangle defined like \"25,25 100x50\")");
+	puts("Options...");
+	puts("-t          Show a timer");
+}
+
 int main (int argc, char *argv[]) {
 	struct nwl_state state = {0};
 	int retval = 1;
+	int opt;
+	while ((opt = getopt(argc, argv, "t")) != -1) {
+		switch (opt) {
+			case 't':
+			rect_show_timer = true;
+			break;
+			default:
+			print_usage(argv[0]);
+			return 0;
+		}
+	}
+	if (argc-optind < 1) {
+		puts("I need to know where to be: \"x,y widthxheight\"");
+		return 1;
+	}
+	int x, y, width, height;
+	if (sscanf(argv[optind], "%i,%i %ix%i", &x, &y, &width, &height) != 4) {
+		puts("I need to know where to be: \"x,y widthxheight\"");
+		return 1;
+	}
 	if (nwl_wayland_init(&state)) {
 		return 1;
 	}
@@ -58,15 +125,7 @@ int main (int argc, char *argv[]) {
 		fprintf(stderr, "No layer shell. I need it.\n");
 		goto finish;
 	}
-	if (argc < 2) {
-		puts("I need to know where to be: \"x,y widthxheight\"");
-		goto finish;
-	}
-	int x, y, width, height;
-	if (sscanf(argv[1], "%i,%i %ix%i", &x, &y, &width, &height) != 4) {
-		puts("I need to know where to be: \"x,y widthxheight\"");
-		goto finish;
-	}
+
 	// Might also want to handle multiple outputs...
 	struct nwl_output *output = find_output(&state, x, y);
 	if (output == NULL) {
@@ -91,11 +150,36 @@ int main (int argc, char *argv[]) {
 	zwlr_layer_surface_v1_set_margin(main_surface->role.layer.wl, y, x2, y2, x);
 	struct wl_region *reg = wl_compositor_create_region(state.wl.compositor);
 	wl_surface_set_input_region(main_surface->wl.surface, reg);
+	if (rect_show_timer) {
+		clock_gettime(CLOCK_MONOTONIC, &time_start);
+		struct nwl_surface *sub = nwl_surface_create(&state, "wlrect sub");
+		int sub_y_pos = y2 < 24 && y > y2 ? -24 : height + 4;
+		nwl_surface_renderer_cairo(sub, false, rect_sub_render);
+		nwl_surface_role_subsurface(sub, main_surface);
+		nwl_surface_set_size(sub, 8*9, 24);
+		wl_subsurface_set_position(sub->role.subsurface.wl, 0, sub_y_pos);
+		nwl_surface_set_need_draw(sub, false);
+		wl_surface_set_input_region(sub->wl.surface, reg);
+		wl_surface_commit(sub->wl.surface);
+		wl_subsurface_set_desync(sub->role.subsurface.wl);
+		time_update_fd = timerfd_create(CLOCK_MONOTONIC, 0);
+		struct itimerspec ts = {
+			.it_interval.tv_sec = 0,
+			.it_interval.tv_nsec = 100000000,
+			.it_value.tv_sec = 0,
+			.it_value.tv_nsec = 100000000
+		};
+		timerfd_settime(time_update_fd, 0, &ts, NULL);
+		nwl_poll_add_fd(&state, time_update_fd, handle_timer, sub);
+	}
 	wl_surface_commit(main_surface->wl.surface);
 	wl_region_destroy(reg);
 	nwl_wayland_run(&state);
 	retval = 0;
 finish:
 	nwl_wayland_uninit(&state);
+	if (time_update_fd == -1) {
+		close(time_update_fd);
+	}
 	return retval;
 }
